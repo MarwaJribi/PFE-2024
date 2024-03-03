5284aff75ec24c39a3c1888c042a6bc66b9685ae
MarwaJribi <marwajribi836@gmail.com>
2024-03-03 11:23:21 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import Home from "./components/home/home";
+
+function App() {
+  return (
+    <div className="App">
+      test
+      <Home />
+    </div>
+  );
+}
+
+export default App;
